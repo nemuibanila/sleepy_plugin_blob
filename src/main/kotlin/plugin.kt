63f148f084b2ba9c7df84155de8959e03c603e0e
NemuiBanila <baniklina3@gmail.com>
2021-01-09 02:30:52 +0100

    money through mining

--- src/main/kotlin/plugin.kt
+++ src/main/kotlin/plugin.kt
@@ -3,23 +3,28 @@ import com.mongodb.client.*
 import com.mongodb.client.model.Filters.eq
 import com.mongodb.client.model.Updates.*
 import org.bson.Document
-import org.bukkit.ChatColor
+import org.bukkit.*
 import org.bukkit.command.*
 import org.bukkit.entity.Player
 import org.bukkit.event.*
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
-import kotlin.math.round
+import kotlin.math.*
 
 const val ADMIN_PERM = "sleepyblob.admin"
 object mongo {
     val client: MongoClient
     val db: MongoDatabase
     val players: MongoCollection<Document>
+    val globals: MongoCollection<Document>
     init {
         client = MongoClients.create()
         db = client.getDatabase("slp_data")
         players = db.getCollection("players")
+        globals = db.getCollection("globals")
+        get_pool() // initialize pool
+        ensure_basic_resources()
     }
 
     fun store_player(uuid: String) {
@@ -62,8 +67,79 @@ object mongo {
     }
 
     fun username_to_uuid(username: String): String? {
-        return "aefe4c36-83d6-43bd-9a24-20522607cc2c";
+        val query = players.find(eq("username", username))
+        if (query.first() != null) return query.first()["uuid"].toString()
+        else return null
     }
+
+    fun get_settings(): Document? {
+        val query = globals.find(eq("name", "settings"))
+        return query.first()
+    }
+
+
+    /*
+    pool:
+    - amount: Double
+     */
+    fun get_pool(): Document {
+        val query = globals.find(eq("name", "pool"))
+        if (query.first() != null) return query.first()
+        else {
+            var pool = Document("name", "pool").append("amount", 0.0)
+            globals.insertOne(pool)
+            return pool
+        }
+    }
+
+    fun save_pool(pool: Document) {
+        globals.replaceOne(eq("name", "pool"), pool)
+    }
+
+    fun pool_add_money(amount: Double) {
+        globals.updateOne(eq("name", "pool"), inc("amount", amount))
+    }
+
+    /* resource pool
+    - material --- index
+    - amount_mined
+    - exp
+     */
+    fun ensure_basic_resources() {
+        ensure_basic_resource(Material.COAL_ORE.createBlockData().asString, 1.0)
+        ensure_basic_resource("minecraft:nether_gold_ore", 1.0)
+        ensure_basic_resource(Material.DIAMOND_ORE.createBlockData().asString, 7.0)
+        ensure_basic_resource(Material.EMERALD_ORE.createBlockData().asString, 7.0)
+        ensure_basic_resource(Material.LAPIS_ORE.createBlockData().asString, 4.0)
+        ensure_basic_resource(Material.NETHER_QUARTZ_ORE.createBlockData().asString, 2.0)
+        ensure_basic_resource(Material.REDSTONE_ORE.createBlockData().asString, 3.0)
+    }
+
+    fun ensure_basic_resource(m: String, worth: Double) {
+        val query = globals.find(eq("material", m))
+        if (query.any()) {
+            return
+        }
+
+        var material_info = Document("material", m)
+        material_info.append("value", worth)
+        material_info.append("mined", 0L)
+        globals.insertOne(material_info)
+    }
+
+    fun mine_resource(m: String): Double {
+        val query = globals.find(eq("material", m))
+        if (query.any()) {
+            var material = query.first()
+            val worth = material["value"] as Double
+            val mined = (material["mined"] as Long).toDouble()
+            globals.updateOne(eq("material", m), inc("mined", 1))
+            return (worth * 1000.0)/(1000.0 + worth*mined)
+        }
+
+        return 0.0
+    }
+
 }
 
 class SleepyBlob : JavaPlugin(), Listener {
@@ -71,6 +147,12 @@ class SleepyBlob : JavaPlugin(), Listener {
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
         getCommand("pay")!!.setExecutor(PayExecutor)
+        getCommand("money")!!.setExecutor(MoneyExecutor)
+    }
+
+    override fun onDisable() {
+        Slp.save_settings()
+        super.onDisable()
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
@@ -93,12 +175,66 @@ class SleepyBlob : JavaPlugin(), Listener {
             mongo.update_player_version(e.player.uniqueId.toString())
         }
     }
+
+    @EventHandler
+    fun onBlockBreak(e: BlockBreakEvent) {
+        if (e.player.isOnline) {
+            println(e.block.blockData.asString)
+            val reward: Double = mongo.mine_resource(e.block.blockData.material.createBlockData().asString)
+            mongo.add_money(e.player.uniqueId.toString(), reward)
+
+            // get current reward
+            // -- need datastore
+            // update amount in store
+
+            // give out reward
+        }
+    }
+}
+
+object Slp {
+    val settings: Document
+    const val settings_version = 1
+    var transaction_fee = 0.2
+
+    init {
+        settings = mongo.get_settings()?: Document("name", "settings")
+        for (kv in settings) {
+            when(kv.key) {
+                "transaction_fee" -> transaction_fee = kv.value as Double
+            }
+        }
+    }
+    const val currency = "Oreru"
+
+    fun balance_str(p: Player): String {
+        return "You currently have ${"%.2f".format(mongo.get_money(p.uniqueId.toString()))} ${currency}s"
+    }
+
+    fun save_settings() {
+        settings["settings_version"] = settings_version
+        settings["transaction_fee"] = transaction_fee
+        if(mongo.get_settings() != null) {
+            mongo.globals.replaceOne(eq("name", "settings"), settings)
+        } else {
+            mongo.globals.insertOne(settings)
+        }
+
+    }
+
+}
+
+object MoneyExecutor : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if(sender is Player) {
+            sender.sendMessage(Slp.balance_str(sender))
+        }
+        return true
+    }
 }
 
 object PayExecutor : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        val cs = sender.server.consoleSender
-
         if (sender is Player) {
             if(args.size < 2)
                 return false
@@ -113,12 +249,12 @@ object PayExecutor : CommandExecutor {
             }
 
             if (uuid == null) {
-                sender.sendMessage("${ChatColor.RED} $target_str could not be found")
+                sender.sendMessage("${ChatColor.RED}$target_str could not be found")
                 return true
             }
 
             if (uuid == sender.uniqueId.toString()) {
-                sender.sendMessage("${ChatColor.RED} You cannot send to yourself")
+                sender.sendMessage("${ChatColor.RED}You cannot send to yourself")
                 return true
             }
 
@@ -127,23 +263,25 @@ object PayExecutor : CommandExecutor {
             var amount = amount_str.toDoubleOrNull() ?: 0.0
             amount = round(amount*100)/100
             if (amount <= 0.0) {
-                sender.sendMessage("${ChatColor.RED} $amount_str is not a valid amount")
+                sender.sendMessage("${ChatColor.RED}$amount_str is not a valid amount")
                 return true
             }
+
+            var fee = Slp.transaction_fee * amount
+            val fee_str = ".2f".format(fee)
             
             // check transaction
             val balance = mongo.get_money(sender.uniqueId.toString())
-            if (balance > amount) {
+            if (balance > amount + fee) {
                 mongo.add_money(sender.uniqueId.toString(), -amount)
                 mongo.add_money(uuid, amount)
-                sender.sendMessage("${ChatColor.GREEN} $amount_str sent to $target_str")
+                mongo.pool_add_money(fee)
+                sender.sendMessage("${ChatColor.GREEN}$amount_str ${Slp.currency} sent to $target_str. Transaction fee: $fee_str ${Slp.currency}s")
             } else {
-                sender.sendMessage("Not enough. Your balance: $balance")
+                sender.sendMessage("${ChatColor.RED}Not enough.  ${Slp.balance_str(sender)} \n" +
+                        "${ChatColor.AQUA}Transaction: $amount + $fee (${(Slp.transaction_fee*100).roundToInt()}%)")
             }
             return true;
-            // apply transaction fee
-
-            // move transaction fee to pool
         }
 
         return false;
